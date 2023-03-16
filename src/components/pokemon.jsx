089778cc3004a838a642f91cf9ab8a474362a05f
Em Nguyen <longemnguyen96@gmail.com>
2023-03-15 20:26:44 -0700

    changed

--- src/components/pokemon.jsx
+++ src/components/pokemon.jsx
@@ -1,31 +1,47 @@
-import React, {useEffect, useState} from "react";
-
+import React, { useEffect, useState } from "react";
 
 const Pokemon = (props) => {
     const [names, setNames] = useState([]);
-
     useEffect(() => {
         fetch("https://pokeapi.co/api/v2/pokemon")
-            .then(response => {
+            .then((response) => {
                 // not the actual JSON response body but the entire HTTP response
                 return response.json();
             })
-            .then(response => {
+            .then((response) => {
                 // we now run another promise to parse the HTTP response into usable JSON
-                setNames(response.results)
+                setNames(response.results);
             })
-            .catch(err => {
+            .catch((err) => {
                 console.log(err);
-            });}, []);
+            });
+    }, []);
+
+    const handleFetchAllPokemon = () => {
+        fetch("https://pokeapi.co/api/v2/pokemon?limit=1118")
+            .then((response) => {
+                // not the actual JSON response body but the entire HTTP response
+                return response.json();
+            })
+            .then((response) => {
+                // we now run another promise to parse the HTTP response into usable JSON
+                setNames(response.results);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
     return (
         <div>
+            <button onClick={handleFetchAllPokemon}>Fetch All Pokemon</button>
             {names.map((pokemon, index) => {
-                    return( 
-                        <div key = {index}>
-                            <p>{pokemon.name}</p>
-                        </div>
-                    );
-                })}
+                return (
+                    <div key={index}>
+                        <p>{pokemon.name}</p>
+                    </div>
+                );
+            })}
         </div>
     );
 };
