3a125813bad870b1d3e033e535819d1cbc31463a
Em Nguyen <longemnguyen96@gmail.com>
2023-03-15 20:19:54 -0700

    first

---
+++ src/components/pokemon.jsx
@@ -0,0 +1,33 @@
+import React, {useEffect, useState} from "react";
+
+
+const Pokemon = (props) => {
+    const [names, setNames] = useState([]);
+
+    useEffect(() => {
+        fetch("https://pokeapi.co/api/v2/pokemon")
+            .then(response => {
+                // not the actual JSON response body but the entire HTTP response
+                return response.json();
+            })
+            .then(response => {
+                // we now run another promise to parse the HTTP response into usable JSON
+                setNames(response.results)
+            })
+            .catch(err => {
+                console.log(err);
+            });}, []);
+    return (
+        <div>
+            {names.map((pokemon, index) => {
+                    return( 
+                        <div key = {index}>
+                            <p>{pokemon.name}</p>
+                        </div>
+                    );
+                })}
+        </div>
+    );
+};
+
+export default Pokemon;
